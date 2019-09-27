f635ea1d3bdd372b979ec76a51bbe35ac6e29362
Matt Jibson <matt.jibson@gmail.com>
2019-09-27 15:34:45 -0600

    fix build

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ go generate
 
 BRANCH=$(git symbolic-ref --short HEAD)
 SHA=$(git rev-parse --short HEAD)
-gcloud --project cockroach-dev-inf builds submit --substitutions=BRANCH_NAME=$BRANCH,SHORT_SHA=$SHA
+gcloud --project cockroach-dev-inf builds submit --config cloudbuild.yaml --substitutions=BRANCH_NAME=$BRANCH,SHORT_SHA=$SHA
 
 #kubectl get po | grep directory | awk '{print $1}' | xargs kubectl delete po
